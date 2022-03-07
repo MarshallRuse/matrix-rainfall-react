688cf2dbc101405cacd021c7c37341b0d0c4c5ea
Marshall Ruse <ruse.marshall@gmail.com>
2022-03-07 08:50:50 -0500

    Completed Matrix code rainfall effect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import MatrixBG from "./MatrixBG";
+
 function App() {
-	return <h1>Matrix Rainfall React - Starting Point</h1>;
+	return (
+		<div style={{ position: "relative", width: "100vw", height: "100vh" }}>
+			<MatrixBG />
+			<h1
+				style={{ color: "#fff", fontSize: "4rem", position: "relative", padding: "2em", pointerEvents: "none" }}
+			>
+				Converting a Vanilla JS Canvas Animation to React
+			</h1>
+		</div>
+	);
 }
 
 export default App;
